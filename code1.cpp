323c3c461336bd25d68814cc3e7c4a055dff0618
mkdev78 <mkdeo78@gmail.com>
2024-08-02 15:39:41 +0530

    Create code1.cpp

---
+++ code1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+typedef struct My_Struct
+{
+    int x;
+    int y;
+} My_Struct_t;
+
+int main()
+{
+    My_Struct_t s1 = {1, 1};
+    My_Struct_t s2 = {1, 2};
+
+    if(s1 == s2)
+    {
+        std::cout << "s1 and s2 have same value" << std::endl;
+    }
+    else
+    {
+        std::cout << "s1 and s2 have different value" << std::endl;
+    }
+}
